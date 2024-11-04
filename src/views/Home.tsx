c50c9f86a870c20c596c883ed7555f07b990a868
rizallhamzahhh <rizalllhamzahh20@gmail.com>
2024-11-04 18:10:36 +0700

    new home

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -396,7 +396,8 @@ export default function Home() {
                   <path d="M22.5 6.908V6.75a3 3 0 0 0-3-3h-15a3 3 0 0 0-3 3v.158l9.714 5.978a1.5 1.5 0 0 0 1.572 0L22.5 6.908Z" />
                 </svg>
               }
-              description="Fashion adalah gaya berpakaian yang populer pada waktu, tempat, dan konteks tertentu"
+              description="galerryekrafkng@co.id"
+              fullWidth
             />
             <Card
               title="No Handphone"
@@ -415,7 +416,8 @@ export default function Home() {
                   />
                 </svg>
               }
-              description="Fashion adalah gaya berpakaian yang populer pada waktu, tempat, dan konteks tertentu"
+              description="08762536234632"
+              fullWidth
             />
             <Card
               title="Alamat"
@@ -431,7 +433,8 @@ export default function Home() {
                   <path d="m12 5.432 8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 0 1-.75-.75v-4.5a.75.75 0 0 0-.75-.75h-3a.75.75 0 0 0-.75.75V21a.75.75 0 0 1-.75.75H5.625a1.875 1.875 0 0 1-1.875-1.875v-6.198a2.29 2.29 0 0 0 .091-.086L12 5.432Z" />
                 </svg>
               }
-              description="Fashion adalah gaya berpakaian yang populer pada waktu, tempat, dan konteks tertentu"
+              description="Jln. Siliwangi"
+              fullWidth
             />
           </div>
         </div>
